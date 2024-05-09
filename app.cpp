25efece3884786fa969c5c91bcf95269892f3d11
mhmd <ms.mhmdsalah@gmail.com>
2024-05-09 05:22:55 +0300

    learn fund by c++

---
+++ app.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+  int num = 10;
+  string name = "mohamed salah";
+  bool status = true;
+  cout << sizeof(num) << endl;
+  cout << sizeof(name) << endl;
+  cout << sizeof(status) << endl;
+
+
+  return 0;
+}
